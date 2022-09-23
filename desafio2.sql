5a3da457933bcf9d6a1ccf298a1c147c93e41c6e
AlanoBurity <alanosantosdev@gmail.com>
2022-09-23 15:05:59 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+	 SELECT
+            ( SElECT COUNT(*) FROM SpotifyClone.musicas ) AS 'cancoes',
+            ( SElECT COUNT(*) FROM SpotifyClone.artistas ) AS 'artistas',
+            ( SElECT COUNT(*) FROM SpotifyClone.albuns ) AS 'albuns';
